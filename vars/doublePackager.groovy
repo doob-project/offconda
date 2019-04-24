3b7d1f845a0dd06f76fdbe20adab823162a72770
Denis Brandolini <denis.brandolini@prometeia.com>
2019-04-24 14:27:27 +0200

    fixup ambiente dipendente dalla envlabel!

--- vars/doublePackager.groovy
+++ vars/doublePackager.groovy
@@ -7,17 +7,17 @@ def call(envlabel, sourcelabel, components, condaenvb="base") {
         unstash "source"
       }
       stage('Generating packages list') {
-        condaShellCmd("conda create -y -n ${CONDAENV} python=2.7", condaenvb)
+        condaShellCmd("conda create -y -n ${CONDAENV}-${envlabel} python=2.7", condaenvb)
         retry(3) {
-            condaShellCmd("conda install --copy -q -c t/${env.ANACONDA_API_TOKEN}/prometeia/channel/${sourcelabel} -c t/${env.ANACONDA_API_TOKEN}/prometeia -c defaults --override-channels --no-channel-priority -y ${components}", "${CONDAENV}")
+            condaShellCmd("conda install --copy -q -c t/${env.ANACONDA_API_TOKEN}/prometeia/channel/${sourcelabel} -c t/${env.ANACONDA_API_TOKEN}/prometeia -c defaults --override-channels --no-channel-priority -y ${components}", "${CONDAENV}-${envlabel}")
         }
         script {
-          writeFile file: "elencone-${envlabel}.txt", text: condaShellCmd("conda list --explicit", "${CONDAENV}", true).trim()
+          writeFile file: "elencone-${envlabel}.txt", text: condaShellCmd("conda list --explicit", "${CONDAENV}-${envlabel}", true).trim()
         }
       }
       stage('ArtifactTearDown') {
         archiveArtifacts artifacts: "elencone-${envlabel}.txt"
-        condaShellCmd("conda env remove -y -n ${CONDAENV}", condaenvb)
+        condaShellCmd("conda env remove -y -n ${CONDAENV}-${envlabel}", condaenvb)
         deleteDir()
       }
     }
